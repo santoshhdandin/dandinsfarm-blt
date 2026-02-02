867da81a6ebc45dbbe601b4eb58455d876adcc8c
santoshhdandin <santoshhdandin@gmail.com>
2026-02-02 08:56:04 +0530

    changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dandinsfarm/', // <-- important for GitHub Pages
+  base: '/', // <-- important for GitHub Pages
 })
